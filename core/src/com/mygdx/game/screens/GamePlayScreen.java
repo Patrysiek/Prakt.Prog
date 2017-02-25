27eb05d6e08525e5f90facafd337072dba02a0eb
Komputer\Patryk <ufekufek3@gmail.com>
2017-02-25 11:55:05 +0100

    GamePlayScreen

---
+++ core/src/com/mygdx/game/screens/GamePlayScreen.java
@@ -0,0 +1,17 @@
+package com.mygdx.game.screens;
+
+import com.mygdx.game.Maingame;
+
+public class GamePlayScreen extends AbstractScreen {
+
+	public GamePlayScreen(Maingame game) {
+		super(game);
+		init();
+	}
+
+	private void init() {
+	
+		
+	}
+
+}
