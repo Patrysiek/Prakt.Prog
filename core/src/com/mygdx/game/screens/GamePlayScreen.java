e887d2fd39a324ef595bbc9db9ff175e95fc4981
Komputer\Patryk <ufekufek3@gmail.com>
2017-02-25 12:23:23 +0100

    dodalismy playera i scene

--- core/src/com/mygdx/game/screens/GamePlayScreen.java
+++ core/src/com/mygdx/game/screens/GamePlayScreen.java
@@ -1,16 +1,38 @@
 package com.mygdx.game.screens;
 
 import com.mygdx.game.Maingame;
+import com.mygdx.game.entities.Player;
 
 public class GamePlayScreen extends AbstractScreen {
 
+	private Player player;
 	public GamePlayScreen(Maingame game) {
 		super(game);
 		init();
 	}
 
 	private void init() {
-	
+	initPlayer();
+		
+	}
+
+	private void initPlayer() {
+		player = new Player();
+		stage.addActor(player);
+		
+	}
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		super.render(delta);
+		update();
+		spriteBatch.begin();
+		stage.draw();//narysuj scene z aktorami 
+		spriteBatch.end();
+	}
+
+	private void update() {
+		stage.act();//rob update co klatke dla wszystkich aktorow
 		
 	}
 
