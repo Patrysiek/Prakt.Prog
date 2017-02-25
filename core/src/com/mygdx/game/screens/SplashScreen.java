714e96e49ae08e1c334feb2ae0d26203b9b3758f
Komputer\Patryk <ufekufek3@gmail.com>
2017-02-25 12:04:44 +0100

    no message

--- core/src/com/mygdx/game/screens/SplashScreen.java
+++ core/src/com/mygdx/game/screens/SplashScreen.java
@@ -1,15 +1,28 @@
 package com.mygdx.game.screens;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.utils.Timer.Task;
 import com.mygdx.game.Maingame;
 
 public class SplashScreen extends AbstractScreen{
 
 	private Texture splashImg;
 	
-	public SplashScreen(Maingame game) {
+	public SplashScreen(final Maingame game) {
 		super(game);
 		init();
+		
+	Timer.schedule(new Task(){
+		
+			@Override
+			public void run(){
+			game.setScreen(new GamePlayScreen(game));
+			
+			}
+		
+		
+		},1);
 	}
 
 	private void init() {
